e402cfe15d883f48dd1df900e64225435acc95f3
Albert Salim <albertlimca@gmail.com>
2016-04-26 14:45:21 +0800

    serve static index.html

--- server.js
+++ server.js
@@ -8,29 +8,29 @@ var port = 3000;
 
 var router = express.Router();
 
-router.route('/')
-.all(function(req,res,next) {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      next();
-})
-.get(function(req,res,next){
-  io.emit('countdown', { message: 'start count down!', duration: 5 });
-  res.end('Hello World!');
-});
+router.route('/countdown')
+  .all(function(req,res,next) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    next();
+  })
+  .get(function(req,res,next){
+    io.emit('countdown', { message: 'start count down!', duration: 5 });
+    res.end('Hello World!');
+  });
 
-router.route('/:minutes')
-.all(function(req,res,next) {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      next();
-})
-.get(function(req,res,next){
-        io.emit('countdown', { message: 'start count down', duration: parseInt(req.params.minutes, 10) })
-        res.end('hello world: minutes:  ' + req.params.minutes +'!');
-})
-;
-app.use('/go', router);
+router.route('/countdown/:minutes')
+  .all(function(req,res,next) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    next();
+  })
+  .get(function(req,res,next){
+    io.emit('countdown', { message: 'start count down', duration: parseInt(req.params.minutes, 10) })
+    res.end('hello world: minutes:  ' + req.params.minutes +'!');
+  });
 
-app.use(express.static(__dirname + '/public'));
+app.use(router);
+
+app.use('/', express.static(__dirname + '/public'));
 
 server.listen(port, hostname, function(){
   console.log(`Server running at http://${hostname}:${port}/`);
