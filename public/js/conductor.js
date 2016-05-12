7412fc136444b7f83d2985d34168b14e1e589d43
Niko <niko.wah.hn@gmail.com>
2016-05-12 16:42:02 +0800

    Added question set to display on conductor html

--- public/js/conductor.js
+++ public/js/conductor.js
@@ -2,4 +2,16 @@ $(function() {
   $("#reset").click(function() {
     $.post("/reset");
   });
+
+  var celloGame = new CelloGame();
+  var celloGameAnswer = celloGame.answer;
+  var celloGameQuestion = celloGame.question;
+  var celloGameQuestionSet = celloGame.questionSet;
+
+  $('#questionSet', '#celloGame').text(celloGameQuestionSet);
+  $('#buttonOne', '#celloGame').text(celloGameQuestion[celloGameAnswer[0]]);
+  $('#buttonTwo', '#celloGame').text(celloGameQuestion[celloGameAnswer[1]]);
+  $('#buttonThree', '#celloGame').text(celloGameQuestion[celloGameAnswer[2]]);
+  $('#buttonFour', '#celloGame').text(celloGameQuestion[celloGameAnswer[3]]);
+
 });
